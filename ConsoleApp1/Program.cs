a9461358d891f5c45bfffc5a2a07041133608a45
Matt Reeds <mattlreed@gmail.com>
2020-10-11 20:01:21 -0500

    string concat

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,20 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
             // Type your username and press enter
-            Console.WriteLine("Enter username:");
+            //Console.WriteLine("Enter username:");
 
             // Create a string variable and get user input from the keyboard and store it in the variable
-            string userName = Console.ReadLine();
+            //string userName = Console.ReadLine();
 
             // Print the value of the variable (userName), which will display the input value
-            Console.WriteLine("Username is: " + userName);
+            //Console.WriteLine("Username is: " + userName);
 
-            Console.WriteLine("Enter your age:");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age is: " + age);
+            //Console.WriteLine("Enter your age:");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your age is: " + age);
+
+            string firstName = "John";
+            string lastName = "Doe";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
         }
     }
 }
